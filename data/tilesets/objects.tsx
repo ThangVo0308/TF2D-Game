9e43b9bc6ca4de389a922da2899159709c5c7c68
ThangVo0308 <thangvo0308@gmail.com>
2024-09-24 14:21:09 +0700

    level 0 map

--- data/tilesets/objects.tsx
+++ data/tilesets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="object" tilewidth="224" tileheight="128" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="224" tileheight="186" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image source="../../graphics/objects/vines/0.png" width="224" height="128"/>
@@ -15,5 +15,8 @@
  </tile>
  <tile id="6">
   <image source="../../graphics/objects/chain_moving/0.png" width="16" height="116"/>
+ </tile>
+ <tile id="7">
+  <image source="../../graphics/objects/flag/0.png" width="68" height="186"/>
  </tile>
 </tileset>
