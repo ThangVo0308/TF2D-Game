d73f6f73f8ae0caba0c26a38ac3e3aa24d462182
qcuong57 <hoquoccuong945@gmail.com>
2024-09-25 19:31:44 +0700

    add Outsite Map

---
+++ data/tilesets/background/bg_outsite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="bg_outsite" tilewidth="64" tileheight="64" tilecount="420" columns="28">
+ <image source="../../../graphics/map/background/bg_outsite.png" width="1800" height="1013"/>
+</tileset>
--- data/tilesets/objects.tsx
+++ data/tilesets/objects.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="224" tileheight="186" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="309" tileheight="576" tilecount="27" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image source="../../graphics/objects/vines/0.png" width="224" height="128"/>
  </tile>
  <tile id="2">
-  <image source="../../graphics/enemies/snake/0.png" width="85" height="64"/>
+  <image source="../../graphics/objects/snake/0.png" width="85" height="64"/>
  </tile>
  <tile id="4">
   <image source="../../graphics/objects/chain.png" width="29" height="70"/>
@@ -18,5 +18,68 @@
  </tile>
  <tile id="7">
   <image source="../../graphics/objects/flag/0.png" width="68" height="186"/>
+ </tile>
+ <tile id="8">
+  <image source="../../graphics/objects/power_line/4.png" width="207" height="576"/>
+ </tile>
+ <tile id="9">
+  <image source="../../graphics/objects/power_line/3.png" width="183" height="576"/>
+ </tile>
+ <tile id="10">
+  <image source="../../graphics/objects/power_line/2.png" width="73" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="../../graphics/objects/power_line/1.png" width="189" height="117"/>
+ </tile>
+ <tile id="12">
+  <image source="../../graphics/objects/tube/1.png" width="150" height="75"/>
+ </tile>
+ <tile id="13">
+  <image source="../../graphics/objects/tube/2.png" width="105" height="66"/>
+ </tile>
+ <tile id="14">
+  <image source="../../graphics/objects/tube/3.png" width="78" height="192"/>
+ </tile>
+ <tile id="15">
+  <image source="../../graphics/objects/tube/4.png" width="309" height="126"/>
+ </tile>
+ <tile id="16">
+  <image source="../../graphics/objects/decor/1.png" width="96" height="51"/>
+ </tile>
+ <tile id="17">
+  <image source="../../graphics/objects/decor/8.png" width="57" height="72"/>
+ </tile>
+ <tile id="18">
+  <image source="../../graphics/objects/decor/13.png" width="171" height="78"/>
+ </tile>
+ <tile id="19">
+  <image source="../../graphics/objects/decor/14.png" width="171" height="78"/>
+ </tile>
+ <tile id="20">
+  <image source="../../graphics/objects/decor/15.png" width="171" height="78"/>
+ </tile>
+ <tile id="21">
+  <image source="../../graphics/objects/decor/16.png" width="171" height="78"/>
+ </tile>
+ <tile id="22">
+  <image source="../../graphics/objects/decor/17.png" width="195" height="48"/>
+ </tile>
+ <tile id="23">
+  <image source="../../graphics/objects/decor/18.png" width="147" height="48"/>
+ </tile>
+ <tile id="24">
+  <image source="../../graphics/objects/decor/19.png" width="96" height="66"/>
+ </tile>
+ <tile id="25">
+  <image source="../../graphics/objects/decor/24.png" width="153" height="171"/>
+ </tile>
+ <tile id="26">
+  <image source="../../graphics/objects/decor/25.png" width="153" height="173"/>
+ </tile>
+ <tile id="27">
+  <image source="../../graphics/objects/decor/26.png" width="204" height="144"/>
+ </tile>
+ <tile id="28">
+  <image source="../../graphics/objects/decor/27.png" width="234" height="105"/>
  </tile>
 </tileset>
