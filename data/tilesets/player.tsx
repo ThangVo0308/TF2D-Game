52dc678cff7e81c0e3cd50f6635631f24c9761f7
Hoài Nam <nthnam.a1.c3tqcap@gmail.com>
2024-10-15 10:23:29 +0700

    Doi ten anh nhan vat

--- data/tilesets/player.tsx
+++ data/tilesets/player.tsx
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="player" tilewidth="52" tileheight="96" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="player" tilewidth="56" tileheight="96" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
-  <image source="../../graphics/player/knight/Idle/1.png" width="52" height="96"/>
+  <image source="../../graphics/player/knight/Idle/1.png" width="56" height="96"/>
  </tile>
  <tile id="4">
-  <image source="../../graphics/player/mage/Idle/1.png" width="50" height="58"/>
+  <image source="../../graphics/player/mage/Idle/0.png" width="50" height="58"/>
  </tile>
  <tile id="5">
-  <image source="../../graphics/player/Rogue/Idle/1.png" width="46" height="50"/>
+  <image source="../../graphics/player/Rogue/Idle/0.png" width="46" height="50"/>
  </tile>
 </tileset>
